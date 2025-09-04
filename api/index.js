916421227d65f9bb426c4c6c5c048e9d773d9a42
salim <salimafzal.shaikh@gmail.com>
2025-09-04 19:36:26 +0530

    update code

--- api/index.js
+++ api/index.js
@@ -1,14 +1,10 @@
 // api/index.js
-const express = require("express");
-const serverless = require("serverless-http");
-
+const express = require('express');
 const app = express();
 
-app.use(express.json());
-
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from Vercel Express API!" });
+app.get('/api', (req, res) => {
+    res.status(200).json('Welcome to your Vercel Node.js API!');
 });
 
+// Important: Export the Express app for Vercel to use as a serverless function
 module.exports = app;
-module.exports.handler = serverless(app);
